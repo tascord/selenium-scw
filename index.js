545cea40e0edcde46446c482c23637f757eff786
flora :3 <imflo.pink@gmail.com>
2024-02-06 14:57:49 +1100

    Change IDs

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const prelude = async () => {
 
   MONITORS.push(...JSON.parse(await (execSync(path.join(__dirname, 'scw.exe'))).toString()));
   console.log(`Found ${MONITORS.length} monitor(s):`);
-  console.log(MONITORS.map(m => `#${m.id}: ${m.width}x${m.height} @ ${m.left},${m.top}`));
+  console.log(MONITORS.map((m, i) => `#${i}: ${m.width}x${m.height} @ ${m.left},${m.top}`));
 
   const group = process.argv[2];
   const monitors = (await $({ url: `https://raw.githubusercontent.com/RMHEDGE/rm-displays/main/group_${group}.json`, parse: 'json' })).body;
